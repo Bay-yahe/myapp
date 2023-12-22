ceda0e02185db77ed97a21cccd2d86cc8193dda4
yujhin <capstonebayahe@gmail.com>
2023-12-22 11:25:44 +0800

    12-22-2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bay_yahe_app/firebase_options.dart';
 import 'package:bay_yahe_app/pages/auth_page.dart';
+import 'package:bay_yahe_app/upload_image.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AuthPage(),
+      home: MyHomePage(),
     );
   }
 }
